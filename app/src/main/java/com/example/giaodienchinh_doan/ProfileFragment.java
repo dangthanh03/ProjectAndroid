fcda660e87808d52a2f0e47b94c657d25b152e3c
dangthanh03 <dangtrithanh03@gmail.com>
2023-10-22 15:39:25 +0700

    Thay đổi thông tin người dùng và lưu trên firebase

--- app/src/main/java/com/example/giaodienchinh_doan/ProfileFragment.java
+++ app/src/main/java/com/example/giaodienchinh_doan/ProfileFragment.java
@@ -1,10 +1,13 @@
 package com.example.giaodienchinh_doan;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,74 +16,35 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ProfileFragment extends Fragment {
+    private static final String TAG = "Update email"; // Declare the TAG constant at the class level
 
     FirebaseAuth auth;
     Button button;
     TextView textView;
     Button Save;
     FirebaseUser user;
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    EditText name;
+    EditText phone;
+    EditText email;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-   EditText name;
-   EditText phone;
-   EditText email;
     public ProfileFragment() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
-
-
-
-    }
-
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -88,54 +52,84 @@ public class ProfileFragment extends Fragment {
 
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
-        name= view.findViewById(R.id.username);
-        phone=view.findViewById(R.id.phone);
+        name = view.findViewById(R.id.username);
+        phone = view.findViewById(R.id.phone);
         email = view.findViewById(R.id.email);
+        email.setEnabled(false);
+        email.setFocusable(false);
+        email.setClickable(false);
+
+
         // Tìm kiếm nút logout trong view được inflate
         Button buttonLogout = view.findViewById(R.id.LogOut);
         Save = view.findViewById(R.id.EditProfile);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        email.setText(user.getEmail());
+        if (user != null) {
+            DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference("users").child(user.getUid());
+            usersRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.exists()) {
+                        User userProfile = snapshot.getValue(User.class);
+                        if (userProfile != null) {
+                            name.setText(userProfile.displayName);
+                            phone.setText(userProfile.phoneNumber);
+                            email.setText(user.getEmail());
+                        }
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Log.e("TAG", "Error retrieving user information: " + error.getMessage());
+                }
+            });
+        }
         Save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-              /*  String email = ((EditText) view.findViewById(R.id.email)).getText().toString();*/
-
-
-                // Assume that mAuth is an instance of FirebaseAuth
-                auth=FirebaseAuth.getInstance();
+                auth = FirebaseAuth.getInstance();
                 FirebaseUser user = auth.getCurrentUser();
                 String uid = user.getUid();
-                String email = user.getEmail();
-                String displayName = name.getText().toString();
-                String phoneNumber =  phone.getText().toString();
+                String mail = email.getText().toString();
 
-// Get a reference to the Firebase Realtime Database
+                String displayName = name.getText().toString();
+                String phoneNumber = phone.getText().toString();
+
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
                 DatabaseReference usersRef = database.getReference("users");
 
-// Create a User object
-                User newUser = new User(email, displayName, phoneNumber);
+                User newUser = new User(mail, displayName, phoneNumber);
 
-// Save the new user with the UID as the key
                 usersRef.child(uid).setValue(newUser)
                         .addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
                             public void onSuccess(Void aVoid) {
-                                // Data successfully saved
                                 Log.d("TAG", "User information saved successfully");
+                                new AlertDialog.Builder(requireContext())
+                                        .setTitle("Success")
+                                        .setMessage("Data has been saved successfully.")
+                                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialogInterface, int i) {
+                                                FragmentTransaction fragmentTransaction = requireActivity().getSupportFragmentManager().beginTransaction();
+                                                fragmentTransaction.detach(ProfileFragment.this);
+                                                fragmentTransaction.attach(ProfileFragment.this);
+                                                fragmentTransaction.commit();
+                                            }
+                                        })
+                                        .show();
                             }
                         })
                         .addOnFailureListener(new OnFailureListener() {
                             @Override
                             public void onFailure(@NonNull Exception e) {
-                                // Error saving data
                                 Log.e("TAG", "Failed to save user information: " + e.getMessage());
                             }
                         });
-
             }
         });
-        // Gán sự kiện click cho nút logout
-
 
         buttonLogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -146,8 +140,7 @@ public class ProfileFragment extends Fragment {
                 getActivity().finish();
             }
         });
+
         return view;
     }
-
-
 }
